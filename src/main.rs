4a0d53140e2e054e888fe9dd79f6eb896f724717
wareya <wareya@gmail.com>
2023-02-15 08:24:59 -0500

    get function calls working

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use alloc::collections::BTreeMap;
 
 use cranelift::prelude::*;
 use cranelift_jit::{JITBuilder, JITModule};
-use cranelift_module::{DataContext, Linkage, Module};
+use cranelift_module::{DataContext, Linkage, Module, FuncId};
 
-use cranelift_codegen::verifier::verify_function;
+use cranelift::codegen::ir::{FuncRef, SigRef};
+use cranelift::codegen::verifier::verify_function;
 
 use parser::ast::ASTNode;
 
@@ -91,7 +92,22 @@ fn main()
     
     let program = Program::new(&ast).unwrap();
     
-    //println!("{:#?}", program);
+    let mut ast_debug = format!("{:#?}", program);
+    ast_debug = ast_debug.replace("    ", " ");
+    ast_debug = ast_debug.split("\n").filter(|x|
+           !x.contains(" position:")
+        && !x.contains(" line:")
+        && !x.contains(" children: None")
+        && !x.contains(" ),")
+        && !x.contains(" },")
+        && !x.contains(" ],")
+        && !x.ends_with(" [")
+        && !x.ends_with(" (")
+        && *x != "}"
+        ).map(|x|
+            x.replace("{}", "None").replace(" {", ":").replace("children: Some(", "children:")
+        ).collect::<Vec<_>>().join("\n");
+    println!("{}\n", ast_debug);
     
     let builder = JITBuilder::new(cranelift_module::default_libcall_names());
     let mut module = JITModule::new(builder.unwrap());
@@ -99,16 +115,16 @@ fn main()
     let mut builder_context = FunctionBuilderContext::new();
     let mut ctx = module.make_context();
     
-    
-    let mut func_ids = BTreeMap::new();
+    let mut func_decs = BTreeMap::new();
     
     for f in &program.funcs
     {
         let f_name = f.0;
         let function = f.1;
         
-        let mut variables = BTreeMap::new();
+        let mut signature = module.make_signature();
         
+        let mut variables = BTreeMap::new();
         let mut parameters = BTreeMap::new();
         {
             for arg in &function.args
@@ -120,7 +136,7 @@ fn main()
                 {
                     "f32" =>
                     {
-                        ctx.func.signature.params.push(AbiParam::new(types::F32));
+                        signature.params.push(AbiParam::new(types::F32));
                     }
                     _ => panic!("asdf"),
                 }
@@ -132,13 +148,27 @@ fn main()
                 parameters.insert(var_name, var_type);
             }
         }
+        // FIXME: multi-return
         match function.return_type.as_str()
         {
-            "f32" => ctx.func.signature.returns.push(AbiParam::new(types::F32)),
+            "f32" => signature.returns.push(AbiParam::new(types::F32)),
             _ => panic!("asdf"),
         }
         
+        let id = module.declare_function(&f_name, Linkage::Export, &signature).unwrap();
+        
+        func_decs.insert(f_name.clone(), (id, signature, variables, parameters, function.return_type.clone()));
+    }
+    
+    for f in &program.funcs
+    {
+        let f_name = f.0;
+        let function = f.1;
+        
+        let (id, signature, mut variables, parameters, _) = func_decs.get(f_name).unwrap().clone();
+        
         let mut builder = FunctionBuilder::new(&mut ctx.func, &mut builder_context);
+        builder.func.signature = signature.clone();
         let block = builder.create_block();
         builder.append_block_params_for_function_params(block);
         builder.switch_to_block(block);
@@ -174,6 +204,7 @@ fn main()
                 let var_type = node.child(0).unwrap().child(0).unwrap().text.clone();
                 let var_name = node.child(1).unwrap().child(0).unwrap().text.clone();
                 let var = Variable::new(i);
+                // FIXME: use actual type
                 builder.declare_var(var, types::F32);
                 if variables.contains_key(&var_name)
                 {
@@ -185,18 +216,41 @@ fn main()
             return false;
         });
         
-        let mut stack = Vec::<(String, Value)>::new(); // 0: type, 1: variable handle
+        let mut funcrefs = BTreeMap::new();
+        function.body.visit(&mut |node : &ASTNode|
+        {
+            let mut i = 0;
+            if node.is_parent() && node.text == "rvarname"
+            {
+                let name = &node.child(0).unwrap().text;
+                if !variables.contains_key(name) && func_decs.contains_key(name)
+                {
+                    let (id, signature, variables, parameters, _) = func_decs.get(name).unwrap();
+                    let funcref = module.declare_func_in_func(*id, builder.func);
+                    let sigref = builder.import_signature(signature.clone());
+                    funcrefs.insert(name.clone(), (funcref, sigref));
+                }
+            }
+            return false;
+        });
+        
         
         struct Environment<'a, 'b, 'c, 'd>
         {
             stack      : &'a mut Vec<(String, Value)>,
+            funcstack  : &'a mut Vec<(FuncRef, SigRef)>,
             variables  : &'a BTreeMap<String, (String, Variable)>,
             builder    : &'b mut FunctionBuilder<'c>,
             program    : &'d Program,
             root_block : &'a Block,
+            func_decs  : &'a BTreeMap<String, (FuncId, Signature, BTreeMap<String, (String, Variable)>, BTreeMap<String, String>, String)>,
+            funcrefs   : &'a BTreeMap<String, (FuncRef, SigRef)>,
         }
         
-        let mut env = Environment { stack : &mut stack, variables : &variables, builder : &mut builder, program : &program, root_block : &block };
+        let mut stack = Vec::new(); // 0: type, 1: variable handle
+        let mut funcstack = Vec::new(); // 0: type, 1: variable handle
+        
+        let mut env = Environment { stack : &mut stack, funcstack : &mut funcstack, variables : &variables, builder : &mut builder, program : &program, root_block : &block, func_decs : &func_decs, funcrefs : &funcrefs };
         fn compile<'a, 'b>(env : &'a mut Environment, node : &'b ASTNode)
         {
             println!("looking at {} to compile!!!", node.text);
@@ -214,6 +268,7 @@ fn main()
                     "return" =>
                     {
                         let mut returns = Vec::new();
+                        // FIXME: check types
                         for child in node.get_children().unwrap()
                         {
                             compile(env, child);
@@ -225,13 +280,66 @@ fn main()
                     {
                         return;
                     }
-                    "rvariable" =>
+                    "rvarname" =>
                     {
                         let name = &node.child(0).unwrap().text;
                         println!("{}", name);
-                        let var = &env.variables[name];
-                        println!("{:?}", var);
-                        env.stack.push((var.0.clone(), env.builder.use_var(var.1)));
+                        if env.variables.contains_key(name)
+                        {
+                            let var = &env.variables[name];
+                            println!("{:?}", var);
+                            env.stack.push((var.0.clone(), env.builder.use_var(var.1)));
+                        }
+                        else if env.funcrefs.contains_key(name)
+                        {
+                            let (funcref, sigref) = env.funcrefs.get(name).unwrap();
+                            env.funcstack.push((*funcref, *sigref));
+                        }
+                        else
+                        {
+                            panic!("unrecognized identifier {}", name);
+                        }
+                    }
+                    "funcargs_head" =>
+                    {
+                        compile(env, node.child(0).unwrap());
+                        let (funcref, sigref) = env.funcstack.pop().unwrap();
+                        
+                        compile(env, node.child(1).unwrap());
+                        let num_args = node.child(1).unwrap().child_count().unwrap();
+                        
+                        let mut args = Vec::new();
+                        // FIXME: check types
+                        for i in 0..num_args
+                        {
+                            let (type_, val) = env.stack.pop().unwrap();
+                            args.push(val);
+                        }
+                        
+                        let funcaddr = env.builder.ins().func_addr(types::I64, funcref);
+                        
+                        let inst = env.builder.ins().call_indirect(sigref, funcaddr, &args);
+                        let results = env.builder.inst_results(inst);
+                        for result in results
+                        {
+                            // FIXME: use actual type
+                            env.stack.push(("f32".to_string(), *result));
+                        }
+                    }
+                    "funcargs" =>
+                    {
+                        for child in node.get_children().unwrap()
+                        {
+                            compile(env, child);
+                        }
+                    }
+                    "number" =>
+                    {
+                        // FIXME: use actual type
+                        let text = &node.child(0).unwrap().text;
+                        let val : f32 = text.parse().unwrap();
+                        let res = env.builder.ins().f32const(val);
+                        env.stack.push(("f32".to_string(), res));
                     }
                     text => 
                     {
@@ -243,6 +351,7 @@ fn main()
                             let op = &node.child(1).unwrap().child(0).unwrap().text;
                             let right = env.stack.pop().unwrap();
                             let left  = env.stack.pop().unwrap();
+                            // FIXME: check types
                             let res = match op.as_str()
                             {
                                 "+" => env.builder.ins().fadd(left.1, right.1),
@@ -275,9 +384,6 @@ fn main()
         builder.seal_all_blocks();
         builder.finalize();
         
-        
-        let id = module.declare_function(&f_name, Linkage::Export, &ctx.func.signature).unwrap();
-        func_ids.insert(f_name.clone(), id);
         module.define_function(id, &mut ctx).unwrap();
         
         let flags = settings::Flags::new(settings::builder());
@@ -291,9 +397,16 @@ fn main()
         module.clear_context(&mut ctx);
     }
     module.finalize_definitions();
-    for (f_name, id) in func_ids.iter()
+    for (f_name, (id, signature, _, _, _)) in func_decs.iter()
     {
         let code = module.get_finalized_function(*id);
-        println!("{:?}", code);
+        // FIXME: SAFETY: use actual function signature
+        let func = unsafe
+        {
+            core::mem::transmute::<_, fn(f32, f32) -> f32>(code)
+        };
+        let a = 3.14;
+        let b = 150.0012;
+        println!("{}({},{}) = {}", f_name, a, b, func(a, b));
     }
 }
