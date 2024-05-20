9d635278be0b6ec45d7c54fa55fc0c384b7035a4
wareya <wareya@gmail.com>
2024-05-20 12:48:23 -0400

    flake update

--- flake.nix
+++ flake.nix
@@ -3,47 +3,72 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = nixpkgs.legacyPackages.${system}.pkgsStatic;
-        fakeCxx = pkgs.runCommand "fakecxx" {} ''
-          install -D ${pkgs.stdenv.cc.cc.lib}/lib/libstdc++.a $out/lib/libc++.a
-        '';
-        konoran = pkgs.rustPlatform.buildRustPackage {
-          pname = "konoran";
-          version = "unstable";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            allowBuiltinFetchGit = true;
+        pkgs = nixpkgs.legacyPackages.${system};
+        mkKonoran =
+          {
+            lib,
+            musl,
+            zlib,
+            ncurses,
+            libxml2,
+            libffi,
+            llvmPackages_18,
+            rustPlatform,
+            static ? false,
+            ...
+          }:
+          let
+            fakeCxx = pkgs.runCommand "fakecxx" { } ''
+              install -D ${pkgs.pkgsStatic.stdenv.cc.cc.lib}/lib/libstdc++.a $out/lib/libc++.a
+            '';
+          in
+          rustPlatform.buildRustPackage {
+            pname = "konoran";
+            version = "unstable";
+            src = self;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              allowBuiltinFetchGit = true;
+            };
+            nativeBuildInputs = [ llvmPackages_18.llvm ];
+            meta.mainProgram = "konoran";
+            RUSTFLAGS =
+              [
+                "-L ${libffi.out}/lib"
+                "-L ${zlib.out}/lib"
+                "-L ${ncurses.out}/lib"
+                "-L ${libxml2.out}/lib"
+              ]
+              ++ lib.optionals static [
+                "-L ${musl.out}/lib"
+                "-L ${fakeCxx.out}/lib"
+                "-C relocation-model=static"
+                "-C target-feature=+crt-static"
+                "-C link-args=-Wl,--no-as-needed"
+                "-C link-args=-static"
+                "-C link-args=${musl.out}/lib/libc.a"
+              ];
           };
-          nativeBuildInputs = with pkgs; [
-            llvmPackages_18.llvm
-          ];
-          RUSTFLAGS = [
-            "-C relocation-model=static"
-            "-C target-feature=+crt-static"
-            "-L ${pkgs.musl.out}/lib"
-            "-L ${pkgs.zlib.out}/lib"
-            "-L ${pkgs.ncurses.out}/lib"
-            "-L ${pkgs.libxml2.out}/lib"
-            "-L ${pkgs.libffi.out}/lib"
-            "-L ${fakeCxx.out}/lib"
-            "-C link-args=-Wl,--no-as-needed"
-            "-C link-args=-static"
-            "-C link-args=${pkgs.musl.out}/lib/libc.a"
-          ];
-
-        };
-       in {
+        # TODO: Not sure why you can't just use pkgs.pkgsStatic.callPackage
+        konoranStatic = mkKonoran ({ static = true; } // pkgs.pkgsStatic);
+        konoran = pkgs.callPackage mkKonoran { };
+      in
+      {
         packages = {
-          inherit konoran;
+          inherit konoran konoranStatic;
           default = konoran;
         };
-        devShell = pkgs.mkShell {
-          inputsFrom = [konoran];
-        };
+        devShell = pkgs.mkShell { inputsFrom = [ konoran ]; };
       }
     );
 }
